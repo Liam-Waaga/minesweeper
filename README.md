bd424400bd05b924b0b9977bd75b1efcecc69e2e
Liamwaaga <liamwaaga@proton.me>
2026-03-16 19:36:46 -0700

    license

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 simple minesweeper game just because i want one
 
 [texture credit](https://gooseninja.itch.io/pixel-minesweeper)
+
+i do not claim copyright of the textures. eveything else, including the code and application (excluding the textures) however, is licensed under the [license](LICENSE.md)
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 
+/* Copyright c 2026 N Liam Waaga */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy of this software */
+/* and associated documentation files (the "Software"), to deal in the Software without */
+/* restriction, sublicense, and/or sell copies of the Software, and to permit persons to whom the */
+/* Software is furnished to do so, subject to the following conditions: */
+/* The above copyright notice and this permission notice shall beincluded in all copies or */
+/* substantial portions of the Software. */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRENTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING */
+/* BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULARE PURPOSE AND */
+/* NONINFRINGMENT. IN NO EVENT SHALL TEH AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, */
+/* DAMAGES OR OTHER LIABILITY< WHETER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING */
+/* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+
+
+
 #include <cstdlib>
 #include <ctime>
 #include <raylib.h>
 
 #include <array>
+#ifndef NDEBUG
 #include <iostream>
+#endif
 
 const unsigned char ZERO_PNG[] = {
 #embed "../resources/0.png"
